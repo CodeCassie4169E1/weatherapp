8f150f3825dadef4cf14854ac1d340743d5c967e
CodeCassie4169E1 <clipcassie@yahoo.com>
2023-08-31 18:27:29 -0500

    fixed javascript
    changes current weather to day of week and searched city to h3 and temp in that city updates upon search

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-let apiKey = "9ee44dd39b241c853f96295c78bd71cb";
-let city = "Bismark";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&${apiKey}`;
-
-function showTemperature(response) {
-  console.log(response.data);
-  let temperature = Math.round(response.data.main.temp);
-  console.log(temperature);
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("input[type='search']");
   let h3 = document.querySelector("h3");
-  h3.innerHTML = city;
-  document.getElementById("now").innerHTML = `${temperature}°`;
+  h3.innerHTML = searchInput.value;
+  let cityName = searchInput.value;
+  let apiKey = "9ee44dd39b241c853f96295c78bd71cb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
 }
 
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temp = document.querySelector("#now");
+  temp.innerHTML = `${temperature}°`;
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", search);
+function displayCurrentDay() {
+  var daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  var currentDate = new Date();
+  var currentDay = currentDate.getDay();
+
+  var dayOfWeekElement = document.getElementById("dayOfWeek");
+  dayOfWeekElement.textContent = daysOfWeek[currentDay];
+}
+
+var searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", displayCurrentDay);
